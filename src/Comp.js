27088539e5ea0f7468d0818c214feed0c62e4ede
Eugene <klev.evzen@gmail.com>
2023-01-03 11:48:00 +0100

    end this project

---
+++ src/Comp.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+function Comp () {
+   return <h2>Hello</h2>
+}
+
+
+export default Comp;
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Comp from "../Comp";
+function App() {
+  return (
+    <>
+      <h1>Hi there!</h1>
+      <Comp />
+    </>
+  );
+}
+
+const message = "Hello!";
+export default App;
+export { message };
